30f38d0d832c8530fb614a11fe35780854c8ced4
robocotani <kotani2022holeinone@gmail.com>
2022-11-09 11:25:44 +0900

    カメラ使用の優先順位を変更

--- ball_only_test.py
+++ ball_only_test.py
@@ -12,68 +12,63 @@ def distance(center_x,center_y):
     #周囲の5点を取って平均
     #かけ離れている点は除外
     dist = []
-    dist1 = depth_frame.get_distance(center_x, center_y)
-    dist2 = depth_frame.get_distance(center_x + 5, center_y + 5)
-    dist3 = depth_frame.get_distance(center_x + 5, center_y - 5)
-    dist4 = depth_frame.get_distance(center_x - 5, center_y + 5)
-    dist5 = depth_frame.get_distance(center_x - 5, center_y - 5)
+    for i in range(5):
+        for j in range(5):
+            dist1 = depth_frame.get_distance(center_x + i, center_y + j)
+            dist.append(float(format(dist1,'.4f')))
     
-    dist.append(float(format(dist1,'.4f')))
-    dist.append(float(format(dist2,'.4f')))
-    dist.append(float(format(dist3,'.4f')))
-    dist.append(float(format(dist4,'.4f')))
-    dist.append(float(format(dist5,'.4f')))
     
     #3.5m以上の点は除外して平均
-    
-    dist_mean = sum(dist)/len(dist)
+    dist_new = [i for i in dist if 0.1 < i < 3.5] 
+
+    dist_mean = sum(dist_new)/len(dist_new)
 
     return dist_mean
 
 
-def right_rotation():
+def right_rotation(duty):
     #右回転(左が正転、右が後転)
     p1.ChangeDutyCycle(duty)
     p2.ChangeDutyCycle(0)
     p3.ChangeDutyCycle(0)
     p4.ChangeDutyCycle(duty)
-    time.sleep(0.05)
-    #停止
-    p1.ChangeDutyCycle(0)
-    p2.ChangeDutyCycle(0)
-    p3.ChangeDutyCycle(0)
-    p4.ChangeDutyCycle(0)
-    time.sleep(0.1)
+    # time.sleep(0.05)
+    # #停止
+    # p1.ChangeDutyCycle(0)
+    # p2.ChangeDutyCycle(0)
+    # p3.ChangeDutyCycle(0)
+    # p4.ChangeDutyCycle(0)
+    # time.sleep(0.1)
 
 
-def left_rotation():
+def left_rotation(duty):
     #左回転(左が後転、右が正転)
     p1.ChangeDutyCycle(0)
     p2.ChangeDutyCycle(duty)
     p3.ChangeDutyCycle(duty)
     p4.ChangeDutyCycle(0)
-    time.sleep(0.05)
-    #停止
-    p1.ChangeDutyCycle(0)
-    p2.ChangeDutyCycle(0)
-    p3.ChangeDutyCycle(0)
-    p4.ChangeDutyCycle(0)
-    time.sleep(0.1)
+    # time.sleep(0.05)
+    # #停止
+    # p1.ChangeDutyCycle(0)
+    # p2.ChangeDutyCycle(0)
+    # p3.ChangeDutyCycle(0)
+    # p4.ChangeDutyCycle(0)
+    # time.sleep(0.1)
 
 
-def move():
+def move(duty):
     #
     p1.ChangeDutyCycle(0)
     p2.ChangeDutyCycle(duty)
     p3.ChangeDutyCycle(0)
     p4.ChangeDutyCycle(duty)
-    time.sleep(0.1)
-    #停止
-    p1.ChangeDutyCycle(0)
-    p2.ChangeDutyCycle(0)
-    p3.ChangeDutyCycle(0)
-    p4.ChangeDutyCycle(0)
-    time.sleep(0.1)
+    # time.sleep(0.1)
+    # #停止
+    # p1.ChangeDutyCycle(0)
+    # p2.ChangeDutyCycle(0)
+    # p3.ChangeDutyCycle(0)
+    # p4.ChangeDutyCycle(0)
+    # time.sleep(0.1)
     
 
 def down():
@@ -113,7 +108,7 @@ p2 = GPIO.PWM(PIN2, 50) #50Hz
 p3 = GPIO.PWM(PIN3, 50) #50Hz
 p4 = GPIO.PWM(PIN4, 50) #50Hz
 
-duty = 80
+duty = 30
             
 p1.start(0) 
 p2.start(0) 
@@ -124,16 +119,20 @@ p4.start(0)
 
 #フラグの設定
 ball_1 = False #ボール位置へ移動 デプスカメラ
-#ball_2 = False #下カメラ
+ball_2 = True #下カメラ
 #cont = False #ボール微調整
 depth = False #距離計測
 hole = False #打球
 
+#画面サイズ設定
+size_w = 1280
+size_h = 720
+
 
 #カメラ設定----------------------------
-#cap = cv2.VideoCapture(0)
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 #cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 #cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
@@ -151,34 +150,43 @@ config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 pipeline = rs.pipeline()
 profile = pipeline.start(config)
 
+# Alignオブジェクト生成
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+print((size_w / 2) -20, (size_w / 2) +20)
+
 try:
     while True:
         # フレーム待ち
-        frames = pipeline.wait_for_frames()
+        #frames = pipeline.wait_for_frames()
         
-        
-        #RGB
-        RGB_frame = frames.get_color_frame()
-        RGB_image = np.asanyarray(RGB_frame.get_data())
+        #座標の補正
+        #aligned_frames = align.process(frames)
+        #color_frame = aligned_frames.get_color_frame()
+        #depth_frame = aligned_frames.get_depth_frame()
+        #if not depth_frame or not color_frame:
+        #    continue
 
-        #depyh
-        depth_frame = frames.get_depth_frame()
-        depth_image = np.asanyarray(depth_frame.get_data())
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.08), cv2.COLORMAP_JET)
+        #RGB_image = np.asanyarray(color_frame.get_data())
+        #depth_image = np.asanyarray(depth_frame.get_data())
+
+        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.08), cv2.COLORMAP_JET)
 
         # 表示
         #images = np.hstack((RGB_image, depth_colormap))
         #cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.rectangle(RGB_image, (0, 600), (1280, 680), (0, 255, 0), 2)
-        cv2.rectangle(RGB_image, (600, 0), (680, 720), (0, 255, 0), 2)
-        cv2.imshow('RealSense', RGB_image)
+        #cv2.rectangle(RGB_image, (0, int(size_h - 80)), (size_w, int(size_h - 40)), (0, 255, 0), 2)
+        #cv2.rectangle(RGB_image, (int((size_w / 2) - 20), 0), (int((size_w / 2) + 20), size_h), (0, 255, 0), 2)
+        #cv2.imshow('RealSense', RGB_image)
+        
         
         #ret,frame2 = cap.read()
+        #cv2.rectangle(frame2, (0, 470), (size_w, 510), (0, 255, 0), 2)
         #cv2.imshow('frame',frame2)
 
-        img1 = RGB_image
+        #img1 = RGB_image
         #img2 = frame2
-        
         
 
         k=cv2.waitKey(1)
@@ -189,53 +197,118 @@ try:
             pipeline.stop()
             break
         if k==ord('s'):#sを押すとスタート
-            ball_1 = True
+            ball_2 = True
             
             #dist = depth_frame.get_distance(center_x, center_y)
             #print(dist)
             
 
         if ball_1 == True:
-            center_ball_1_x,center_ball_1_y = M_ball.ball_detect(img1)
+            frames = pipeline.wait_for_frames()
+            aligned_frames = align.process(frames)
+            color_frame = aligned_frames.get_color_frame()   
+            cv2.rectangle(RGB_image, (0, int(size_h - 80)), (size_w, int(size_h - 40)), (0, 255, 0), 2)
+            cv2.rectangle(RGB_image, (int((size_w / 2) - 20), 0), (int((size_w / 2) + 20), size_h), (0, 255, 0), 2)
+            cv2.imshow('RealSense', RGB_image)     
+            img1 = RGB_image
+
+
+            upper_leht_1_x,upper_leht_1_y,center_ball_1_x,center_ball_1_y,img1 = M_ball.ball_detect(img1)
+            #cv2.imshow('RealSense', img1)
 
             if center_ball_1_x == None:
-                    #ボール未検出
-                    #ball_2 = True
+                #ボール未検出
+                #ball_1 = False
+                #ball_2 = True
+                right_rotation()
                 print("未検出")
                 pass
-            elif center_ball_1_x < 600:                    
+            elif center_ball_1_x < (size_w / 2) - 20:                    
                 left_rotation() #左回転
                 print('left')
-            elif center_ball_1_x > 680:                    
+            elif center_ball_1_x > (size_w / 2) + 20:                    
                 right_rotation() #右回転
                 print('right')
-            elif 600 <= center_ball_1_x <= 680:
+            elif (size_w / 2) - 20 <= center_ball_1_x <= (size_w / 2) + 20:
                 if center_ball_1_y == None:
+                    ball_1 = False
+                    ball_2 = True
+                    print("未検出")
                     #ボール未検出
                     pass
-                elif center_ball_1_y > 680:
+                elif center_ball_1_y > size_h - 40:
                     down() #後転
                     print('down')
-                elif center_ball_1_y < 600:                        
+                elif center_ball_1_y < size_h - 80:                        
                     move() #正転
                     print('move')
-                elif 600 <= center_ball_1_y <= 680:
+                elif size_h - 80 <= center_ball_1_y <= size_h - 40:
                     #範囲に入ったら停止
                     print('stop')
                     ball_1 = False
-                    hole = True
+                    ball_2 = True
                 else:
                     pass
-          
+
+
+        if ball_2 == True:
+            ret,frame2 = cap.read()
+
+            img2 = frame2
+
+            upper_left_2_x,upper_left_2_y,center_ball_2_x,center_ball_2_y,img2 = M_ball.ball_detect(img2)
+
+            cv2.rectangle(frame2, (int((size_w / 2) - 20), 0), (int((size_w / 2) + 20), size_h), (0, 255, 0), 2)
+            cv2.rectangle(frame2, (0, 470), (size_w, 510), (0, 255, 0), 2)
+            cv2.imshow('frame', img2)
+
+            print(center_ball_2_x)
+
+            if center_ball_2_x == None:
+                #ボール未検出
+                #right_rotation()
+                ball_1 = True
+                ball_2 = False
+                print("未検出")
+                pass
+            elif center_ball_2_x < (size_w / 2) -20:                    
+                left_rotation(10) #左回転
+                print('left2')
+            elif center_ball_2_x > (size_w / 2) +20:                    
+                right_rotation(10) #右回転
+                print('right2')
+            elif (size_w / 2) - 20 <= center_ball_2_x <= (size_w / 2) + 20:
+                if upper_left_2_y == None:
+                    #ボール未検出,後退
+                    down()
+                    pass
+                elif upper_left_2_y > 490:
+                    down() #後転
+                    print('down2')
+                elif upper_left_2_y < 490:                        
+                    move(30) #正転
+                    print('move2')
+                elif 470 <= upper_left_2_y <= 510:
+                    #範囲に入ったら停止
+                    print('stop2')
+                    ball_1 = False
+                    ball_2 = False
+                    hole = True
+                    p1.ChangeDutyCycle(0)
+                    p2.ChangeDutyCycle(0)
+                    p3.ChangeDutyCycle(0)
+                    p4.ChangeDutyCycle(0)
+                else:
+                    pass
+
+            for i in range(5):
+                cap.read()
+            
             
         if hole == True:
 	
-            dist_depth = distance(center_ball_1_x,center_ball_1_y)
-            
-            print(dist_depth)
             print("打球")
 
-             #if k==ord('d'):
             hole = False
             ball_1 = True
 
